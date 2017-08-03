a15e75d6d77e4927897533f1c3291ad0e1cad688
Ruben_fr <ruben.f.alcaraz@gmail.com>
2017-08-04 01:44:32 +0200

    Actualización

---
+++ ConsoleApp1/Difference.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDifference
+{
+    class Difference
+    {
+        /// <summary>
+        /// Método que devuelve los elementos diferneciales entre dos listas utilizando method syntax.
+        /// </summary>
+        public static IEnumerable<int> DifferenceMethodSyntax(List<int> list1, List<int> list2)
+        {
+            if (list1.Count == 0)
+            {
+                throw new ArgumentException("List1 cannot be empty");
+            }
+            if (list2.Count == 0)
+            {
+                throw new ArgumentException("List2 cannot be empty");
+            }
+            return list1.Where(number => !list2.Contains(number));
+        }
+        /// <summary>
+        /// Método que devuelve los elementos diferneciales entre dos listas utilizando query syntax.
+        /// </summary>
+        public static IEnumerable<int> DifferenceQuerySyntax(List<int> list1, List<int> list2)
+        {
+            if (list1.Count == 0)
+            {
+                throw new ArgumentException("List1 cannot be empty");
+            }
+            if (list2.Count == 0)
+            {
+                throw new ArgumentException("List2 cannot be empty");
+            }
+            /*var difference = from number in list1
+                         join r in list2 
+                         on number equals r into numbers
+                         where !numbers.Any()
+                         select number;*/
+            var differences = from number in list1
+                              where !list2.Contains(number)
+                              select number;
+            return differences;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ListDifference
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var list1 = new List<int>()
+            {
+                1,2,3
+            };
+            var list2 = new List<int>()
+            {
+                2,3
+            };
+
+            var difference = Difference.DifferenceMethodSyntax(list1,list2);
+
+            Console.WriteLine("Difference between lists (Method Syntax):");
+            foreach (var element in difference)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+
+            var differences = Difference.DifferenceQuerySyntax(list1,list2);
+
+            Console.WriteLine("Difference between lists (Query syntax):");
+            foreach (var element in differences)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+
         }
     }
 }
